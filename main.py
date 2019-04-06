7140827d40b3dc20e8db22584bc45e8e5ce031ba
M E <matthias.engh@gmail.com>
2019-04-06 17:25:57 -0500

    details

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import random
 fen_string = sys.argv[1]
 
 board = chess.Board(fen_string)
-move = random.sample(list(board.legal_moves),1)[0]
+moves = list(board.legal_moves)
+
+move = random.sample(moves,1)[0]
 board.push(move)
 
 print board
