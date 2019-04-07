0557893f541bc0139177ee0709ecee1aebf308d7
M E <matthias.engh@gmail.com>
2019-04-07 17:42:45 -0500

    print board instead

--- cli.py
+++ cli.py
@@ -1,9 +1,10 @@
 import sys
 import chesscomp
+import chess
 
 # main target, random
 fen_string = sys.argv[1]
 
 new_fen = chesscomp.make_move(fen_string)
 
-print(new_fen)
+print(chess.Board(new_fen))
