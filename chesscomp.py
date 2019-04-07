9067ecfd6ae5f02b23a71bb12013c27fe34c0250
M E <matthias.engh@gmail.com>
2019-04-07 03:17:41 -0500

    further refactor

--- chesscomp.py
+++ chesscomp.py
@@ -2,10 +2,16 @@ import re
 import chess
 import random
 
-def evaluate_moves_by_options(board):
+def evaluate_moves(board, evaluator):
   moves = iter(board.legal_moves)
   for move in moves:
-    yield apply_metric(board, move)
+    yield apply_metric(board, move, evaluator)
+
+def maximize(evaluated_moves):
+  current_result = next(evaluated_moves)
+  for move_evaluation in evaluated_moves:
+    current_result = min_result(current_result, move_evaluation)
+  return current_result
 
 def minimize(evaluated_moves):
   current_result = next(evaluated_moves)
@@ -14,24 +20,37 @@ def minimize(evaluated_moves):
   return current_result
 
 def find_move_with_fewest_opponent_options(board):
-  evaluated_moves = evaluate_moves_by_options(board)
-  result = minimize(evaluated_moves)
+  moves_with_options = evaluate_moves(board, count_moves)
+  result = minimize(moves_with_options)
   return result[0]
 
-def evaluate_position(board):
+def find_move_with_max_next_turn_options(board):
+  moves_with_minimized_options = (board)
+  result = maximize(moves_with_minimized_options)
+  return result[0]
+
+def count_moves(board):
   return len(list(board.legal_moves))
 
-def evaluate_move(board, move):
+def evaluate_move(board, move, evaluator):
   board.push(move)
-  evaluation = evaluate_position(board)
+  evaluation = evaluator(board)
   board.pop()
   return evaluation
 
-def apply_metric(board, move):
-  return [move, evaluate_move(board, move)]
+def apply_metric(board, move, evaluator):
+  return [move, evaluate_move(board, move, evaluator)]
 
 def min_result(result1, result2):
-  if result1[1] < result2[1]: # minimizes opponent moves
+  if result1[1] < result2[1]: # minimizes result
+    return result1
+  elif result1[1] == result2[1]:
+    return random.choice([result1, result2])
+  else:
+    return result2
+
+def max_result(result1, result2):
+  if result1[1] > result2[1]: # max result
     return result1
   elif result1[1] == result2[1]:
     return random.choice([result1, result2])
--- docs.py
+++ docs.py
@@ -2,19 +2,21 @@ import chesscomp
 import doctest
 import chess
 
-def apply_metric(board, move):
+def apply_metric(*args, **kwargs):
   """ returns a result formatted [move, evaluation]
     >>> board = chess.Board()
+    >>> def evaluator(board):
+    ...   return 1
     >>> move = next(iter(board.legal_moves))
-    >>> result = apply_metric(board, move)
+    >>> result = apply_metric(board, move, evaluator)
     >>> type(result[1])
     <class 'int'>
     >>> result[0] == move
     True
   """
-  return chesscomp.apply_metric(board, move)
+  return chesscomp.apply_metric(*args, **kwargs)
 
-def make_move(fen_string):
+def make_move(*args, **kwargs):
   """ takes fen as argument and returns different fen
     >>> fen_string = chess.Board().fen()
     >>> new_fen = make_move(fen_string)
@@ -23,6 +25,6 @@ def make_move(fen_string):
     >>> chess.Board(new_fen).is_valid()
     True
   """
-  return chesscomp.make_move(fen_string)
+  return chesscomp.make_move(*args, **kwargs)
 
 doctest.testmod()
