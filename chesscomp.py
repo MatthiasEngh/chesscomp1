fc7d29c43e7263ed8f1629bb53740a0e90409332
M E <matthias.engh@gmail.com>
2019-07-02 23:42:16 -0500

    deextract legal_moves

--- chesscomp.py
+++ chesscomp.py
@@ -18,10 +18,6 @@ def evaluate_nodes(nodes, evaluator):
   for node in nodes:
     yield [node[0], evaluator(node)]
 
-def legal_moves(board):
-  for move in board.legal_moves:
-    yield [move, board]
-
 def node_value(move_and_value):
   return move_and_value[1]
 
@@ -36,7 +32,10 @@ def make_move(pgn_string):
     move = all_legal_moves[i]
     board.push(move)
 
-    node_evaluations = evaluate_nodes(legal_moves(board), count_moves_eval)
+    node_evaluations = evaluate_nodes(
+      [[move, board] for move in board.legal_moves],
+      count_moves_eval
+    )
     current_result = next(node_evaluations)
     for evaluation in node_evaluations:
       if current_result[1] < evaluation[1]:
