84d42a99823809a67644056fe275535c2e2a6c63
MatthiasEngh <matthias@wescrimmage.com>
2019-07-03 10:55:41 -0500

    rename update_fen to new_position

--- chesscomp.py
+++ chesscomp.py
@@ -7,9 +7,9 @@ def mate_or_minimum(response_counts):
   return len(response_counts) and min(response_counts) or 9001
 
 def move_minimum(fen):
-  return mate_or_minimum([count_responses(update_fen(fen, move)) for move in chess.Board(fen).legal_moves])
+  return mate_or_minimum([count_responses(new_position(fen, move)) for move in chess.Board(fen).legal_moves])
 
-def update_fen(fen, move):
+def new_position(fen, move):
   board = chess.Board(fen)
   board.push(move)
   return board.fen()
@@ -17,5 +17,5 @@ def update_fen(fen, move):
 def make_move(fen):
   moves = list(chess.Board(fen).legal_moves)
   random.shuffle(moves)
-  evaluations = [move_minimum(update_fen(fen, move)) for move in moves]
+  evaluations = [move_minimum(new_position(fen, move)) for move in moves]
   return moves[evaluations.index(max(evaluations))]
