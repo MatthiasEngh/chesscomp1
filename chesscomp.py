b54aa4bb6d1bde16b0326d4946ab7c457f25b3e2
M E <matthias.engh@gmail.com>
2019-07-03 00:23:25 -0500

    extract function move_minimum

--- chesscomp.py
+++ chesscomp.py
@@ -12,29 +12,28 @@ def count_responses(move, board):
   board.pop()
   return move_count
 
+def move_minimum(move1, board):
+  board.push(move1)
+
+  next_turn_options_count = [[move2, count_responses(move2, board)] for move2 in board.legal_moves]
+  result = next_turn_options_count[0][1]
+  for evaluation in next_turn_options_count[1:]:
+    if result < evaluation[1]:
+      pass
+    elif result == evaluation[1]:
+      result = random.choice([result, evaluation[1]])
+    else:
+      result = evaluation[1]
+
+  board.pop()
+  return result
+
 def make_move(pgn_string):
   pgn = io.StringIO(pgn_string)
   game = chess.pgn.read_game(pgn)
   board = game.board()
 
-  all_legal_moves = list(board.legal_moves)
-  next_turn_options_count_minima = [None] * len(all_legal_moves)
-  for i in range(len(all_legal_moves)):
-    board.push(all_legal_moves[i])
-
-    next_turn_options_count = [[move, count_responses(move, board)] for move in board.legal_moves]
-
-    worst_response_with_value = next_turn_options_count[0]
-    for evaluation in next_turn_options_count[1:]:
-      if worst_response_with_value[1] < evaluation[1]:
-        pass
-      elif worst_response_with_value[1] == evaluation[1]:
-        worst_response_with_value = random.choice([worst_response_with_value, evaluation])
-      else:
-        worst_response_with_value = evaluation
-
-    next_turn_options_count_minima[i] = [all_legal_moves[i], worst_response_with_value[1]]
-    board.pop()
+  next_turn_options_count_minima = [[move, move_minimum(move, board)] for move in board.legal_moves]
 
   best_move_with_value = next_turn_options_count_minima[0]
   for evaluation in next_turn_options_count_minima[1:]:
