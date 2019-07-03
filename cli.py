fc0d4c4de1d01b1a313a4e5e3e74355407f3a2ae
MatthiasEngh <matthias@wescrimmage.com>
2019-07-03 11:55:34 -0500

    clean up cli

--- cli.py
+++ cli.py
@@ -1,14 +1,5 @@
-import chess
 import chesscomp
-import io
 import sys
 
 # main target, random
-pgn_string = sys.argv[1]
-
-pgn = io.StringIO(pgn_string)
-game = chess.pgn.read_game(pgn)
-
-move = chesscomp.make_move(game.board())
-
-print(move)
+print(chesscomp.make_move(sys.argv[1]))
