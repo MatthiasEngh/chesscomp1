a94e57fa82b105cd50a705511e3a49c366bed4e2
M E <matthias.engh@gmail.com>
2019-07-03 02:11:43 -0500

    pass around fens

--- chesscomp.py
+++ chesscomp.py
@@ -3,23 +3,22 @@ import chess
 import chess.pgn
 import random
 
-def count_responses(board):
-  return len(list(board.legal_moves))
+def count_responses(fen):
+  return len(list(chess.Board(fen).legal_moves))
 
 def mate_or_minimum(response_counts):
   return len(response_counts) and min(response_counts) or 9001
 
-def move_minimum(board):
-  return mate_or_minimum([on_updated_board_do(board, move, count_responses) for move in board.legal_moves])
+def move_minimum(fen):
+  return mate_or_minimum([on_updated_board_do(fen, move, count_responses) for move in chess.Board(fen).legal_moves])
 
-def on_updated_board_do(board, move, routine):
+def on_updated_board_do(fen, move, routine):
+  board = chess.Board(fen)
   board.push(move)
-  result = routine(board)
-  board.pop()
-  return result
+  return routine(board.fen())
 
-def make_move(board):
-  moves = list(board.legal_moves)
+def make_move(fen):
+  moves = list(chess.Board(fen).legal_moves)
   random.shuffle(moves)
-  evaluations = [on_updated_board_do(board, move, move_minimum) for move in moves]
+  evaluations = [on_updated_board_do(fen, move, move_minimum) for move in moves]
   return moves[evaluations.index(max(evaluations))]
