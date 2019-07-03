0a9471cdb63c6c5159da9c27e2bf15a4bc8fa40f
M E <matthias.engh@gmail.com>
2019-07-03 01:35:53 -0500

    knows how to mate

--- chesscomp.py
+++ chesscomp.py
@@ -3,16 +3,24 @@ import chess
 import chess.pgn
 import random
 
-def count_responses(move, board):
+def legal_responses(move, board):
   board.push(move)
-  move_count = len(list(board.legal_moves))
+  result = list(board.legal_moves)
   board.pop()
+  return result
+
+def count_responses(move, board):
+  move_count = len(legal_responses(move, board))
   return move_count
 
+def mate_or_minimum(response_counts):
+  if len(response_counts):
+    return min(response_counts)
+  else:
+    return 9001
+
 def move_minimum(move1, board):
-  board.push(move1)
-  result = min([count_responses(move2, board) for move2 in board.legal_moves])
-  board.pop()
+  result = mate_or_minimum([count_responses(move2, board) for move2 in legal_responses(move1, board)])
   return result
 
 def make_move(board):
